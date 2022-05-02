af2eca2c8da431147267c7e3a2b3ca8786866b21
Dukke0 <eriduma@gmail.com>
2022-05-02 12:59:53 -0400

    added file utils functions

--- Model/utils.py
+++ Model/utils.py
@@ -1,5 +1,7 @@
 
-import pandas as pd 
+import pandas as pd
+import os, glob
+import time
 
 #Folder and files
 folder_name = "temp"
@@ -17,4 +19,32 @@ def parse_networks_file(filename):
     print(df)
     print(df.to_dict())
     return ['A','B','C']
+
+def set_temp(name):
+    global folder_name
+    folder_name = name
+
+def temp_folder():
+    current_path = os.getcwd()
+    try:
+        os.mkdir(current_path + '/temp')
+        return 0
+    except FileExistsError as e:
+        return 1
+
+def delete_file(file):
+   for f in glob.glob(file + "*"):
+      os.remove(f)
+
+def delete_temp():
+    current_path = os.getcwd()
+    for root, dirs, files in os.walk(current_path + '/temp', topdown=False):
+        for name in files:
+            os.remove(os.path.join(root, name))
+        for name in dirs:
+            os.rmdir(os.path.join(root, name))
+    try:
+        os.rmdir(current_path + '/temp')
+    except:
+        return 1
 
